3d40f96c2a2c5aee02bc39e8718355727f63b76e
nabin171 <karki0008@gmail.com>
2026-06-17 20:06:41 +0545

    if else has been completed

--- dictionary/01dict.py
+++ dictionary/01dict.py
@@ -1,11 +1,18 @@
-dictt={
-    "ram":0,
-    "hari":"43",
-    "shyam":"233"
-    }
+# dictt={
+#     "ram":0,
+#     "hari":"43",
+#     "shyam":"233"
+#     }
 
+# dictt["nabin"]="20"
+# print(dictt.keys())
+# print(dictt.values())
+# dictt.update({"ram":"100","shyam":"2"})
+# print(dictt.items())
 
-print(dictt.keys())
-print(dictt.values())
-dictt.update({"ram":"100","shyam":"2"})
-print(dictt.items())
+# thisdoict=dict(ram="32",shyam="23")
+# print(thisdoict)
+
+s={1,5,32}
+e=set()
+print(type(e))
---
+++ sets/1.py
@@ -0,0 +1,8 @@
+# s={1,2,323,23,232,32,3,"Harry"}
+# print(s, type(s))
+# s.add("hari")
+# print(s)
+s=set()
+# 
+
+print(s)
