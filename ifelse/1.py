c96375a01249fe5ca12aed3cb0707df7e9ba592b
nabin171 <karki0008@gmail.com>
2026-06-21 21:40:51 +0545

    revised from basic

--- ifelse/1.py
+++ ifelse/1.py
@@ -1,8 +1,3 @@
-x=int(input("enter the first number"))
-y=int(input("enter the second number"))
-z=int(input("enter the third number"))
-avg=((x+y+z)/300)*100;
-if (avg>=40 and  x>=33 and y>=33 and z>=33):
-    print("pass")
-else:
-    print("fail")    
+num= int(input("ENter a number please"))
+for i in range(1,11):
+    print("num  x i", num*i)
