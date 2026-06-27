dced8f403b5db2b708c1c191c8db0145e031e6be
nabin171 <karki0008@gmail.com>
2026-06-27 07:01:40 +0545

    praticed hte loop tutorials

--- ifelse/2.py
+++ ifelse/2.py
@@ -1,2 +1,7 @@
-s=set()
-print(type(s))
+for i in range(400):
+    pass
+
+i=0
+while(i<5):
+    print(i)
+    i+=1
---
+++ loop/1.py
@@ -0,0 +1,24 @@
+#multipliaction table of number using the foor loop
+# num=int(input("Enter the number please"))
+# for i in range(1,11):
+#     print(i*num)
+
+
+# l=["harry","soham","sachin","rahul"]
+# for i in l:
+#     if i.startswith("s"):
+#         print("Hello",i
+
+
+# num=int(input("Enter the numebrs"))
+# i=1
+# while(i<11):
+#    print(i*num) 
+#    i+=1
+
+
+
+# write a program to find whether a given numbers is prime or not?
+num=int("Enter the numebers")
+isPrim=True
+if(num<=1)
